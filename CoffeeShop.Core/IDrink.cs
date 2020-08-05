4061cb67fcbd4c8f69a9f9796ab3f612ff87c339
azorzovilis <alexzorzov@gmail.com>
2020-08-05 21:29:53 +0100

    Added unit tests

---
+++ CoffeeShop.Core/IDrink.cs
@@ -0,0 +1,13 @@
+﻿namespace CoffeeShop.Core
+{
+    public interface IDrink
+    {
+        string Title { get; set; }
+
+        double BasePrice { get; set; }
+
+        int LoyaltyPointsGained { get; set; }
+
+        double BaseCost { get; set; }
+    }
+}
